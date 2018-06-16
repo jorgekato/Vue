37c8cddec61212fef85af69f32d2ee54b872f270
Jorge Kato <katojorge@hotmail.com>
2018-06-16 12:07:36 -0300

    first commit.

---
+++ alurapic/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h1>{{ titulo }}</h1>
+
+<ul>
+  <li v-for ="foto of fotos">
+    <img v-bind:src="foto.url" :alt="foto.titulo"></li>
+  
+</ul>
+
+   
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return{
+      titulo: 'Alurapic',
+      fotos: [
+        {
+        url: 'https://images-na.ssl-images-amazon.com/images/I/51Ya2eTZVPL.jpg',
+        titulo: 'livro'
+      },
+      {
+        url: 'https://images-na.ssl-images-amazon.com/images/I/51Ya2eTZVPL.jpg',
+        titulo: 'livrao'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
